a4ccadb9c5e018c9503fe6b938bceb048f1e5f15
plofts <80458159+plofts@users.noreply.github.com>
2022-06-29 10:59:34 +0000

    Support for both 1.7.3 and 2.0.0a

--- pexels/__init__.py
+++ pexels/__init__.py
@@ -1,6 +1,10 @@
+import discord
 from .pexels import Pexels
 
 
 async def setup(bot):
     cog = Pexels(bot)
-    bot.add_cog(cog)
+    if discord.__version__ == "1.7.3":
+        bot.add_cog(cog)
+    else:
+        await bot.add_cog(cog)
